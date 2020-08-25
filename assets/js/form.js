12e191fa8dd5a436bdd67808d81d6c2ce1498de3
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-08-25 18:05:19 -0300

    Add checkout page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,5 @@
 (function () {
-  const inputNumbers = $('.form .form__input-number');
-  inputNumbers.each((index, element) => {
+  $('.form .form__input-number').each((index, element) => {
     const $element = $(element);
 
     const input = $element.find('input[type="number"]');
@@ -17,5 +16,19 @@
       const value = Number(input.val());
       input.val(value > 0 ? value - 1 : 0);
     });
+  });
+
+  $('.form__section--radio').each((index, element) => {
+    const $element = $(element);
+    const labels = $element.find('.form__section--radio__labels > *');
+    $(labels[1]).css('display', 'none');
+
+    const inputs = $element.find('input[type="radio"]');
+    const callback = () => {
+      $(labels[0]).css('display', 'none');
+      $(labels[1]).css('display', 'block');
+      inputs.off('change', callback);
+    };
+    inputs.change(callback);
   });
 })();
